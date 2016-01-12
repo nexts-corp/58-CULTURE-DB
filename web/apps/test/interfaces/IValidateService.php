91b5d16616f9eb4a3c82b3d3df30975a5fbb927b
tawatchai.w <tawatchai.w@tawatchaiw-PC.nexts-corp.com>
2016-01-12 10:11:48 +0700

    jquery validation http://www.formvalidator.net/

---
+++ web/apps/test/interfaces/IValidateService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace apps\test\interfaces;
+
+/**
+ * @name ValidateService
+ * @uri /validate
+ * @description ValidateService
+ */
+interface IValidateService {
+
+    /**
+     * @name index
+     * @uri /index
+     * @description  index
+     */
+    public function index();
+
+}
