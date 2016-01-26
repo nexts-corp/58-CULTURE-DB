9d51bd628c00de7cf8bbd239a25ae4f42b612be7
Panya Noipram <panya.n@baanprogram.com>
2016-01-26 15:16:01 +0700

    Consider Inform All

---
+++ web/apps/consider/interfaces/IConsiderInformSubstituteService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace apps\consider\interfaces;
+
+/**
+ * @name IConsiderInformSubstituteService
+ * @uri /cInformSubsti
+ * @description พิจารณาคำขอใบแทนใบอนุญาต
+ */
+interface IConsiderInformSubstituteService {
+
+    /**
+     * @name view
+     * @uri /view
+     * @description แสดงหน้าพิจารณาคำขอใบแทนใบอนุญาต
+     **/
+    public function viewConsiderInformSubstitute();
+    
+
+    
+}
