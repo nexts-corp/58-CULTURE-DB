c655b532e1d4c5e5cc5599eba8a81e57c40cda2b
somchit chanudom <somchit.c@baanprogram.com>
2016-01-27 02:41:16 +0700

    #update i18n

--- web/apps/home/widget/NavWidget.php
+++ web/apps/home/widget/NavWidget.php
@@ -6,6 +6,9 @@ use th\co\bpg\cde\core\CWidget;
 use th\co\bpg\cde\collection\CJView;
 use th\co\bpg\cde\collection\CJViewType;
 use th\co\bpg\cde\core\impl\CServiceLoaderImpl;
+use th\co\bpg\cde\core\impl\ChangdaoEngineImpl;
+use th\co\bpg\cde\collection\impl\CI18NImpl;
+use th\co\bpg\cde\collection\impl\CViewLoader;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -26,12 +29,31 @@ class NavWidget extends CWidget {
         foreach ($apps as $i => $value) {
             $list_apps[] = $loader->load($value);
         }
-        
+       
+
         $view = new CJView("nav", CJViewType::HTML_VIEW_ENGINE);
-        $view->route = $this->getRoute();
-        $view->routes = $this->getApplication()->routeTables;
-        $view->apps=$list_apps;
-        return $view;
+        $view->apps = $list_apps;
+        $datax=[];
+        $datax["apps"]=$list_apps;
+        $cc=$this->render2($datax, "home");
+        return $cc;
+    }
+
+    public function render2($datax,$appId) {
+        $this->_context_path = \th\co\bpg\cde\core\impl\ChangdaoEngineImpl::$_CONTEXT_PATH;
+        $loaders = array("apps/" . $appId . '/views', "views");
+        if (!is_dir("views")) {
+            $loaders = array("apps/" . $appId . '/views');
+        }
+        $options = array('extension' => '.html');
+        $m = new \Mustache_Engine(array(
+            'loader' => new CViewLoader($loaders, $options),
+            'charset' => 'UTF-8',
+            'helpers' => array(
+                
+            )
+        ));
+        return $m->render("nav", $datax);
     }
 
 }
